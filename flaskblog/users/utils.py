f6f9b1f241beec4204a92cf31829169ccbbf7b14
mrugankray <mrugankray2@gmail.com>
2020-10-20 22:23:30 +0530

    fixed password reset

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -25,7 +25,7 @@ def reset_email(user):
                   sender="mrugankray@gmail.com",
                   recipients=[f"{user.email}"])
     msg.body = f'''To reset your password, visit the following link:
-{url_for('reset_token', token=token, _external=True)}
+{url_for('users.reset_token', token=token, _external=True)}
 
 If you did not make this request then simply ignore this email and no changes will be made.
 '''
