dd468bb2bf9be9ea52a39971dd69057a12a1975e
mrugankray <mrugankray2@gmail.com>
2020-10-21 01:03:17 +0530

    using SSL instead of TLS

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
